560feffdb5e6b9c643a7d8815f09b4e474474788
Joseph Childress <joseph@crowdflower.com>
2012-11-09 19:29:51 -0800

    app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'b81e58b50a27078f6a35b65845a49d846f953b48fbb2d4c428be36fbdbd7008767fb62bbe9082a7bee67dc24204e37878c65abb079636faee8f681070d8de31d'
